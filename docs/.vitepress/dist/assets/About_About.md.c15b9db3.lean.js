ba3c5a2fecdaa7a7df4fea9aea296d170ef4937b
SeongwooJo <swjo0517@gmail.com>
2023-10-26 23:34:23 -0400

    build website

---
+++ docs/.vitepress/dist/assets/About_About.md.c15b9db3.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,V as a}from"./chunks/framework.410404a1.js";const m=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"About/About.md","filePath":"About/About.md"}'),n={name:"About/About.md"},r=a("",11),s=[r];function i(u,l,h,c,d,p){return o(),t("div",null,s)}const g=e(n,[["render",i]]);export{m as __pageData,g as default};
